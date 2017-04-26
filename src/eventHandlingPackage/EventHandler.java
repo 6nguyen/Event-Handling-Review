98d93e894b182ff1846a18002408640a7a9ecadf
George Nguyen <g.nguyen92@yahoo.com>
2017-04-26 16:20:34 -0700

    Add check boxes and ItemListener events.

--- src/eventHandlingPackage/EventHandler.java
+++ src/eventHandlingPackage/EventHandler.java
@@ -1,8 +1,11 @@
 package eventHandlingPackage;
 
 import java.awt.FlowLayout;				// imports layout
+import java.awt.Font;					// imports text fonts
 import java.awt.event.ActionListener;	// imports action listener
 import java.awt.event.ActionEvent;		// imports types of events (clicks, hovers, enter, etc)
+import java.awt.event.ItemEvent;		// imports item type events
+import java.awt.event.ItemListener;		// imports item listener
 import java.awt.Insets;					// imports button margins
 
 import javax.swing.JFrame;				// imports a customizable window
@@ -12,6 +15,7 @@ import javax.swing.JOptionPane;			// imports display window
 import javax.swing.JButton;				// imports a customizable button
 import javax.swing.Icon;				// imports an icon
 import javax.swing.ImageIcon;			// imports an image
+import javax.swing.JCheckBox;			// imports a check box
 
 
 
@@ -25,12 +29,18 @@ public class EventHandler extends JFrame{
 	private JButton button1;
 	private JButton imageButton;
 	
+	private JTextField checkBoxText;
+	private JCheckBox boldBox;
+	private JCheckBox italicBox;
+	
 // CONSTRUCTOR =======================================
 	
 	public EventHandler(){
+		
 		super("Window Title");						// create window title
 		setLayout(new FlowLayout());				// creates layout
 		
+		// TEXT FIELDS ==============================
 		item1 = new JTextField(10);					// length of JTextField is 10
 		add(item1);									// adds item to window
 	
@@ -44,9 +54,10 @@ public class EventHandler extends JFrame{
 		passwordField = new JPasswordField("password");	// "password" appears in asterisks
 		add(passwordField);
 		
+		// BUTTONS ==================================
+		
 		button1 = new JButton("Normal Button");			// create a simple button with text
 		add(button1);
-		
 		
 		String path1 = "C:\\Users\\gnguy\\Desktop\\Java\\EventHandling\\src\\eventHandlingPackage\\icon1.png";
 		String path2 = "C:\\Users\\gnguy\\Desktop\\Java\\EventHandling\\src\\eventHandlingPackage\\icon2.png";
@@ -60,7 +71,22 @@ public class EventHandler extends JFrame{
 		imageButton.setRolloverIcon(icon2);					// set icon2 to appear on hover
 		add(imageButton);
 		
+		// CHECKBOX ==============================
+		
+		checkBoxText = new JTextField
+				("Check the boxes below for a surprise!", 25);
+		checkBoxText.setFont(new Font("Serif", Font.PLAIN, 14));
+		add(checkBoxText);
+		
+		boldBox = new JCheckBox("Bold");
+		add(boldBox);
+		italicBox = new JCheckBox("Italic");
+		add(italicBox);
+		
+		// EVENT HANDLER OBJECTS ==========================
 		// Declare myHandler object before creating the myHandler class
+		
+		//  Action Listener Handler ===============
 		myHandler eventHandler = new myHandler();
 		item1.addActionListener(eventHandler);
 		item2.addActionListener(eventHandler);
@@ -69,6 +95,12 @@ public class EventHandler extends JFrame{
 		
 		button1.addActionListener(eventHandler);
 		imageButton.addActionListener(eventHandler);
+		
+		// Item Listener Handler ===============
+		itemHandler handler = new itemHandler();
+		boldBox.addItemListener(handler);
+		italicBox.addItemListener(handler);
+			
 		
 	} // End of EventHandler Constructor
 	
@@ -95,7 +127,7 @@ public class EventHandler extends JFrame{
 		public void actionPerformed(ActionEvent event){		
 			
 			String fieldString = "";						
-			
+			// Text fields ==========================
 			if(event.getSource()==item1){
 				fieldString = String.format("field 1: %s", event.getActionCommand());
 			} 
@@ -108,14 +140,39 @@ public class EventHandler extends JFrame{
 			else if (event.getSource()==passwordField){
 				fieldString = String.format("password field is : %s", event.getActionCommand());
 			}
-			
+			// Buttons ============================
 			else if (event.getSource()==button1){
 				fieldString = String.format("You clicked %s!", event.getActionCommand());
 			}
 			else if (event.getSource()==imageButton){
 				fieldString = String.format("You clicked Image Button!");
 			}
+			// Check Boxes ========================
+			
 			JOptionPane.showMessageDialog(null, fieldString);
+		}
+	}
+	
+	private class itemHandler implements ItemListener{
+		
+		public void itemStateChanged(ItemEvent event){
+			
+			Font font = null;		// stores the font of the text field we will modify
+			
+			if(boldBox.isSelected() && italicBox.isSelected()){
+				font = new Font("Serif", Font.BOLD + Font.ITALIC, 14);
+			}
+			else if (boldBox.isSelected()){
+				font = new Font("Serif", Font.BOLD, 14);
+			}
+			else if (italicBox.isSelected()){
+				font = new Font("Serif", Font.ITALIC, 14);
+			}
+			else {
+				font = new Font("Serif", Font.PLAIN, 14);
+			}
+			
+			checkBoxText.setFont(font);
 		}
 	}
 }
